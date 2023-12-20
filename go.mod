a00e6068cb8816703761b30a5e9a6cffd3b3b997
0xcatrovacer <swarnabgarang@gmail.com>
2023-12-20 20:40:53 +0530

    [feat]: Added RabbitMQ producer

--- go.mod
+++ go.mod
@@ -5,5 +5,4 @@ go 1.20
 require (
 	github.com/google/uuid v1.5.0
 	github.com/rabbitmq/amqp091-go v1.9.0
-	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee
 )
--- pkg/rabbitmq/producer.go
+++ pkg/rabbitmq/producer.go
@@ -1,1 +1,43 @@
 package rabbitmq
+
+import (
+	"fmt"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
+type Producer struct {
+	connection *amqp091.Connection
+	channel    *amqp091.Channel
+	queue      string
+}
+
+func NewProducer(amqpURL string, queueName string) (*Producer, error) {
+	connection, err := amqp091.Dial(amqpURL)
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to RabbitMQ: %w", err)
+	}
+
+	channel, err := connection.Channel()
+	if err != nil {
+		return nil, fmt.Errorf("error creating channel: %w", err)
+	}
+
+	_, err = channel.QueueDeclare(
+		queueName,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error declaring queue: %w", err)
+	}
+
+	return &Producer{
+		connection: connection,
+		channel:    channel,
+		queue:      queueName,
+	}, nil
+}
