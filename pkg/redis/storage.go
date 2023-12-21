a7ea325e507dec9c275fd61d0b4b5f0ab1eec9da
0xcatrovacer <swarnabgarang@gmail.com>
2023-12-21 21:42:49 +0530

    [cleanup]

--- pkg/redis/storage.go
+++ pkg/redis/storage.go
@@ -12,7 +12,6 @@ type Task struct {
 	CpuLoad       int       `json:"cpu_load"`
 	DiskLoad      int       `json:"disk_load"`
 	MemoryLoad    int       `json:"memory_load"`
-	BandwidthLoad int       `json:"bandwidth_load"`
 	ExecutionTime int       `json:"execution_time"`
 }
 
