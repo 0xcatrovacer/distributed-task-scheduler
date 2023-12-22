da9fe5f11b9ce1144df47d2122e604f03e1ad887
0xcatrovacer <swarnabgarang@gmail.com>
2023-12-22 22:18:42 +0530

    [feat]: Added interfaces to avoid import cycles

---
+++ pkg/common/interfaces.go
@@ -0,0 +1,21 @@
+package common
+
+import (
+	"distributed-task-scheduler/pkg/models"
+)
+
+type MessageHandler interface {
+	HandleMsg(msg *models.Message)
+}
+
+type MessagePublisher interface {
+	PublishMessage(message string, routingKey string) error
+}
+
+type TaskScheduler interface {
+	ScheduleTask(msg *models.Message) error
+}
+
+type TaskExecutor interface {
+	HandleTaskExecution(msg *models.Message)
+}
