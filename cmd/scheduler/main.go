2eb66832745087638d2dbd6fa24f58d627034605
0xcatrovacer <swarnabgarang@gmail.com>
2023-12-23 20:24:16 +0530

    [feat]: Added scheduler logic to schedule tasks

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"distributed-task-scheduler/pkg/rabbitmq"
+	"distributed-task-scheduler/pkg/scheduler"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err.Error())
+	}
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDRESS"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       0,
+	})
+
+	amqpURL := fmt.Sprintf("amqp://%s:%s@%s:%s/",
+		os.Getenv("RABBITMQ_DEFAULT_USER"),
+		os.Getenv("RABBITMQ_DEFAULT_PASS"),
+		os.Getenv("RABBITMQ_HOST"),
+		os.Getenv("RABBITMQ_PORT"),
+	)
+
+	taskRegistryQueue := "task_registry_queue"
+	taskExchange := "task_registry_exchange"
+	routingKey := "task_reg"
+
+	consumer, err := rabbitmq.NewConsumer(amqpURL, taskRegistryQueue, taskExchange, routingKey)
+	if err != nil {
+		log.Fatalf("Failed to create consumer: %s", err)
+	}
+
+	producer, err := rabbitmq.NewProducer(amqpURL, "schedule_exchange")
+	if err != nil {
+		log.Fatalf("error creating RabbitMQ producer: %s", err)
+	}
+
+	taskScheduler := scheduler.New(redisClient, producer)
+
+	consumer.Consume(taskScheduler, nil)
+}
--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -1,12 +1,94 @@
 package scheduler
 
-import "github.com/google/uuid"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
 
-type Message struct {
-	ID   uuid.UUID
-	Type string
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
+
+	"distributed-task-scheduler/pkg/models"
+	"distributed-task-scheduler/pkg/rabbitmq"
+)
+
+type Scheduler struct {
+	redisClient *redis.Client
+	producer    *rabbitmq.Producer
 }
 
-func HandleTaskRegistry(msg Message) {
-	//TODO: To be implemented
+func New(redisClient *redis.Client, producer *rabbitmq.Producer) *Scheduler {
+	return &Scheduler{
+		redisClient: redisClient,
+		producer:    producer,
+	}
+}
+
+func (s *Scheduler) ScheduleTask(msg *models.Message) error {
+	var value models.TaskRegistryMessageValue
+
+	if err := json.Unmarshal(msg.Value, &value); err != nil {
+		return fmt.Errorf("error unmarshalling task registry message: %w", err)
+	}
+
+	taskRegistryMsg := models.TaskRegistryMessage{
+		ID:    msg.ID,
+		Type:  msg.Type,
+		Value: value,
+	}
+
+	task, err := s.fetchTask(taskRegistryMsg.Value.TaskID.String())
+	if err != nil {
+		return fmt.Errorf("error fetching task from Redis: %w", err)
+	}
+
+	serverID, err := s.chooseServer()
+	if err != nil {
+		return fmt.Errorf("error choosing server: %w", err)
+	}
+
+	scheduleMsgVal := models.ScheduleMessageValue{
+		TaskID:   taskRegistryMsg.Value.TaskID,
+		ServerID: uuid.MustParse(serverID),
+	}
+
+	scheduleMsg := models.ScheduleMessage{
+		ID:    uuid.New(),
+		Type:  "TASK_EXECUTE",
+		Value: scheduleMsgVal,
+	}
+
+	msgBytes, err := json.Marshal(scheduleMsg)
+	if err != nil {
+		return fmt.Errorf("error marshaling schedule message: %w", err)
+	}
+
+	routingKey := serverID
+	if err := s.producer.PublishMessage(string(msgBytes), routingKey); err != nil {
+		return fmt.Errorf("error publishing schedule message: %w", err)
+	}
+
+	log.Printf("Scheduled task %s to server %s", task.ID, serverID)
+
+	return nil
+}
+
+func (s *Scheduler) fetchTask(taskID string) (*models.Task, error) {
+	taskData, err := s.redisClient.Get(context.Background(), "task:"+taskID).Result()
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving task from Redis: %w", err)
+	}
+
+	var task models.Task
+	if err := json.Unmarshal([]byte(taskData), &task); err != nil {
+		return nil, fmt.Errorf("error unmarshaling task data: %w", err)
+	}
+	return &task, nil
+}
+
+func (s *Scheduler) chooseServer() (string, error) {
+	// TODO: Implement Server selection logic
+
+	return uuid.New().String(), nil
 }
