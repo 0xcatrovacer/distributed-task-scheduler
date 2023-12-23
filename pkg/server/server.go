bfbb79687368fd0dcf36db6a57e9badf76abe901
0xcatrovacer <swarnabgarang@gmail.com>
2023-12-24 03:36:22 +0530

    [feat]: Added logic to execute task

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -4,23 +4,15 @@ import (
 	"context"
 	"distributed-task-scheduler/pkg/models"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/google/uuid"
 )
-
-type ServerStatus struct {
-	ID                uuid.UUID `json:"id"`
-	CpuUtilization    int       `json:"cpu_utilization"`
-	CpuLimit          int       `json:"cpu_limit"`
-	MemoryUtilization int       `json:"mem_utilization"`
-	MemoryLimit       int       `json:"mem_limit"`
-	DiskUtilization   int       `json:"disk_utilization"`
-	DiskLimit         int       `json:"disk_limit"`
-}
 
 type Server struct {
 	redisClient *redis.Client
@@ -35,16 +27,83 @@ func New(redisClient *redis.Client, serverID uuid.UUID) *Server {
 }
 
 func (s *Server) HandleTaskExecution(msg *models.Message) error {
-	// TODO: Implement task fetching and execution logic
-	// TODO: Implement new compute info calculation logic
+	var value models.ScheduleMessageValue
 
-	newCpuUtil := 0
-	newMemUtil := 0
-	newDiskUtil := 0
+	if err := json.Unmarshal(msg.Value, &value); err != nil {
+		return fmt.Errorf("error unmarshalling schedule message: %w", err)
+	}
+
+	scheduleMsg := models.ScheduleMessage{
+		ID:    msg.ID,
+		Type:  msg.Type,
+		Value: value,
+	}
+
+	serverID := scheduleMsg.Value.ServerID
+	if serverID != s.serverID {
+		return fmt.Errorf("task not assigned to this server")
+	}
+
+	serverStat, err := s.fetchServer(serverID.String())
+	if err != nil {
+		return fmt.Errorf("could not fetch server")
+	}
+
+	cpuUtil := serverStat.CpuUtilization
+	diskUtil := serverStat.DiskUtilization
+	memUtil := serverStat.MemoryUtilization
+
+	taskID := scheduleMsg.Value.TaskID
+
+	task, err := s.fetchTask(taskID.String())
+	if err != nil {
+		return fmt.Errorf("could not fetch task")
+	}
+
+	newCpuUtil := cpuUtil + task.CpuLoad
+	newMemUtil := memUtil + task.MemoryLoad
+	newDiskUtil := diskUtil + task.DiskLoad
 
 	s.UpdateComputeInfo(s.serverID, newCpuUtil, newMemUtil, newDiskUtil)
 
+	execution := time.After(time.Duration(task.ExecutionTime))
+
+	<-execution
+
+	newCpuUtil -= task.CpuLoad
+	newMemUtil -= task.MemoryLoad
+
+	s.UpdateComputeInfo(s.serverID, newCpuUtil, newMemUtil, newDiskUtil)
+
+	// TODO: Add logic to send to completed queue
+
 	return nil
+}
+
+func (s *Server) fetchTask(taskID string) (*models.Task, error) {
+	taskData, err := s.redisClient.Get(context.Background(), "task:"+taskID).Result()
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving task from Redis: %w", err)
+	}
+
+	var task models.Task
+	if err := json.Unmarshal([]byte(taskData), &task); err != nil {
+		return nil, fmt.Errorf("error unmarshaling task data: %w", err)
+	}
+	return &task, nil
+}
+
+func (s *Server) fetchServer(serverID string) (*models.ServerStatus, error) {
+	serverData, err := s.redisClient.Get(context.Background(), "server:"+serverID).Result()
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving server from Redis: %w", err)
+	}
+
+	var serverStat models.ServerStatus
+	if err := json.Unmarshal([]byte(serverData), &serverStat); err != nil {
+		return nil, fmt.Errorf("error unmarshaling task data: %w", err)
+	}
+	return &serverStat, nil
 }
 
 func (s *Server) UpdateInitialComputeInfo() {
@@ -59,7 +118,7 @@ func (s *Server) UpdateInitialComputeInfo() {
 		log.Println("unable to fetch initial compute info")
 	}
 
-	computeInfo := ServerStatus{
+	computeInfo := models.ServerStatus{
 		ID:                s.serverID,
 		CpuUtilization:    cpuUtil,
 		CpuLimit:          cpuLimit,
@@ -82,7 +141,7 @@ func (s *Server) UpdateInitialComputeInfo() {
 }
 
 func (s *Server) UpdateComputeInfo(serverID uuid.UUID, newCpuUtil int, newMemUtil int, newDiskUtil int) {
-	var computeInfo *ServerStatus
+	computeInfo := &models.ServerStatus{}
 
 	redata, err := s.redisClient.Get(context.Background(), "server:"+serverID.String()).Result()
 	if err != nil {
